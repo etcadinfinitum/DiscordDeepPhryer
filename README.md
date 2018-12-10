91ba3f4c677218c644e8ca7cfbbf69e85c6e9089
Lizzy Presland <lizzy.presland@gmail.com>
2018-12-10 09:58:22 +0000

    Setting up bot basic functionality. Help message and single attachment responses implemented. I had trouble tracking down the options to use `aiohttp` to retrieve a bytestream of the resource behind the URL instead of a wrapper object. `aiohttp` is the preferred method for file retrieval (instead of `requests`) because it is asyncronous. I need to dig into the docs further to figure it out.
    Preliminary tests work, which is cool!

---
+++ README.md
@@ -0,0 +1,18 @@
+# Deep Phryer
+
+*A Discord bot built in Python*
+
+### Get your own Phryer
+
+Go through the steps to create and authorize a bot on a Discord server. Steps are described [here](https://www.devdungeon.com/content/make-discord-bot-python), among other places.
+
+The script expects your bot's authorization token to be in a file called `creds.py`. Create this file, and add a single line like so:
+```TOKEN='XXXXXX_YOUR_TOKEN_HERE_XXXXXXXXX'```
+
+The bot requires Wand and imagemagick to be installed. Make sure you have the necessary packages installed; read the installation docs [here](http://docs.wand-py.org/en/0.4.1/guide/install.html).
+
+### Credits
+
+This bot is licensed under GPL v3.0. Go forth and propogate, but do not privatize. See the LICENSE docs for more information.
+
+Huge props to [Finn](https://github.com/thefinn93) for building most of the foundation for the bot and for letting me add support for GIFs. The original bot code is [here](https://git.callpipe.com/finn/deep-fried-sigger).
---
+++ bot.py
@@ -0,0 +1,120 @@
+# run with python3
+import discord
+import creds 
+import pdb
+from wand.image import Image
+import tempfile
+import math
+import random
+import logging
+from urllib.request import urlretrieve
+import os
+import aiohttp
+import requests
+
+
+client = discord.Client()
+
+def create_params():
+    frequency = random.randint(1, 10) # 3
+    phase_shift = random.randint(-90, -25)  #-90
+    amplitude = random.randint(1, 9) * 0.1 # 0.2
+    bias = random.randint(1, 9) * 0.1 # 0.7
+    params = [frequency, phase_shift, amplitude, bias]
+    return params
+
+def heckin_rainbows(img):
+    params = create_params()
+    img.function('sinusoid', params)
+    return params
+
+def mkgif(filename):
+    outfile = tempfile.mkstemp(prefix="deepfry", suffix=".gif")[1]
+    logging.info("About to run mkgif(%s) -> %s", filename, outfile)
+    with Image() as final:
+        with Image(filename=filename) as img:
+            params = create_params()
+            for frame in img.sequence:
+                with Image(frame).convert('jpeg') as mod_frame:
+                    mod_frame.modulate(brightness=150, saturation=200)
+                    slope = math.tan((math.pi * (55/100.0+1.0)/4.0))
+                    if slope < 0.0:
+                        slope=0.0
+                    intercept = 15/100.0+((100-15)/200.0)*(1.0-slope)
+                    mod_frame.function("polynomial", [slope, intercept])
+                    mod_frame.evaluate(operator='gaussiannoise', value=0.05)
+                    mod_frame.function('sinusoid', params)
+                    with mod_frame.convert('gif') as new_frame:
+                        mod_frame.delay = 3
+                        final.sequence.append(new_frame)
+        final.type = 'optimize'
+        final.save(filename=outfile)
+    return outfile, None
+
+def deepfry(filename):
+    outfile = tempfile.mkstemp(prefix="deepfry", suffix=".jpg")[1]
+    logging.info("About to run deepfry(%s) -> %s", filename, outfile)
+    with Image(filename=filename) as img:
+        img.modulate(brightness=150, saturation=200)
+        slope = math.tan((math.pi * (55/100.0+1.0)/4.0))
+        if slope < 0.0:
+          slope=0.0
+        intercept = 15/100.0+((100-15)/200.0)*(1.0-slope)
+        img.function("polynomial", [slope, intercept])
+        img.evaluate(operator='gaussiannoise', value=0.05)
+        params = heckin_rainbows(img)
+        img.save(filename=outfile)
+    response = ""
+    response += "Parameters:\nFrequency: {}\nPhase shift: {}\nAmplitude: {}\nBias: {}\n".format(*params)
+    return outfile, response
+
+
+@client.event
+async def on_message(message):
+    # bot do not reply to bot, ya dig?
+    if message.author==client.user:
+        return
+    
+    # help messages are good ^^,
+    if message.content.startswith('deepfriedHELP'):
+        msg = 'Welcome to THE DEEP FRYER.\nUse your powers wisely.\n\nCommands:\tAttachment:\tResult:\nFryThis\t\t\tjpg, png\t\t\tHecking FRIED\nFryThis\t\t\tNone\t\t\t\tTry it!\n\nBETA FEATURES:\n'
+        await client.send_message(message.channel, msg)
+    
+    # FRY THIS
+    if message.content.startswith('FryThis'):
+        # the sender sent an attachment! whoop whoop
+        if len(message.attachments) > 0:
+            print('only frying first image for the time being')
+            if not os.path.isdir('/tmp/phryer'):
+                os.mkdir('/tmp/phryer')
+            filename = '/tmp/phryer/{}'.format(message.attachments[0]['filename'] + '.jpg')
+            # retrieve the attachment
+            img_data = requests.get(message.attachments[0]['url'], stream=True)
+            with open(filename, 'wb') as f:
+                for chunk in img_data.iter_content(chunk_size=128):
+                    f.write(chunk)
+            # block comment is for aiohttp which is the discord.py recommendation but I can't figure out how to get bytestreams of the content from the request response
+            '''
+            with aiohttp.ClientSession() as session:
+                async with session.get(message.attachments[0]['url']) as resp:
+                    pdb.set_trace()
+                    # useful properties: resp.status, resp.headers['Content-Type'], etc
+                    with open(filename, 'wb') as the_file:
+                        the_file.write(resp.read())
+            pdb.set_trace()
+            '''
+            # deep fry the resulting attachment
+            result_file, text = deepfry(filename)
+            await client.send_file(message.channel, result_file, content=text, filename="test.jpg")
+        # the sender did not send an attachment; oops
+        else:
+            print('no image attached; sending a stock image instead')
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------------')
+
+client.run(creds.TOKEN)
