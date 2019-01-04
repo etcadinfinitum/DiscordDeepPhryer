f37c3c9e5b431b76bf26848542848998c1698171
Lizzy <lizzy.presland@gmail.com>
2019-01-03 16:19:39 -0800

    Working on emoji reactions (see issue #7) Had to restructure a little bit to limit code duplication, hence the gigantic diff.

--- bot.py
+++ bot.py
@@ -158,6 +158,24 @@ def parse_args(content):
     return operations
 
 @client.event
+async def on_reaction_add(reaction, user):
+    if reaction.emoji == '😈':
+        # respond - requested a fry!
+        logging.info('detected reaction with smiling_imp emoji; trigger the fry')
+        img_result, text = await bot_response(reaction.message, True)
+        if not img_result:
+            await client.send_message(reaction.message.channel, text)
+        else:
+            destination_channel = client.get_channel(creds.DUMP_CHANNEL)
+            if not destination_channel:
+                destination_channel = reaction.message.channel
+                logging.info('Predefined destination channel is not available for this fry; sending in original channel')
+            else:
+                logging.info('Predefined destination channel IS AVAILABLE for this fry')
+            await client.send_file(destination_channel, img_result, content=text, filename='derp' + os.path.splitext(img_result)[1])
+            
+
+@client.event
 async def on_message(message):
     # bot do not reply to bot, ya dig?
     if message.author==client.user:
@@ -185,76 +203,82 @@ async def on_message(message):
         emojis = {'robot': '🤖', 'camera': '📸'}
         for item in emojis.values():
             await client.add_reaction(message, item)
-        # the sender sent an attachment! whoop whoop
-        if len(message.attachments) > 0:
-            logging.info('Detected an attachment to fry!')
-            logging.info('only frying first image for the time being')
-            if not os.path.isdir('/tmp/phryer'):
-                os.mkdir('/tmp/phryer')
-            result_file = '/tmp/phryer/{}'.format(message.attachments[0]['filename'])
-            logging.info('result file is: ' + result_file)
-            # retrieve the attachment
-            is_gif = False
-            is_valid = True
-            # useful properties: resp.status, resp.headers['Content-Type'], etc
-            with aiohttp.ClientSession() as session:
-                async with session.get(message.attachments[0]['url']) as resp:
-                    # check response for content-type attribute
-                    if resp.headers['Content-Type'] == 'image/gif':
-                        is_gif = True
-                    elif resp.headers['Content-Type'] != 'image/png' and resp.headers['Content-Type'] != 'image/jpeg':
-                        is_valid = False
-                    # read response into file
-                    if is_valid:
-                        with open(result_file, 'wb') as the_file:
-                            async for line in resp.content:
-                                the_file.write(line)
-            # deep fry the resulting attachment
-            send_attachment = True
-            operations = parse_args(message.content)
-            if is_valid:
-                if is_gif:
-                    result_file, text = mkgif(result_file, operations)
-                else:
-                    result_file, text = deepfry(result_file, operations)
-                if os.path.getsize(result_file) > 7800000:
-                    send_attachment = False
-                    text += '\nThe file is too large to attach; RIP. '
-                if operations['chaos']:
-                    send_attachment = False
-                    text += ' Be advised: clicking the link may cause eye strain.'
-                if operations['link'] or not send_attachment:
-                    if not creds.SUPPORT_LINKS:
-                        text += 'Harass your admin to get image links supported.'
-                    else:
-                        text += make_link(result_file)
-                if send_attachment:
-                    logging.info('sent text response: %s' % text)
-                    await client.send_file(message.channel, result_file, content=text, filename='test' + os.path.splitext(result_file)[1])
-                else:
-                    logging.info('sent text response: %s' % text)
-                    await client.send_message(message.channel, text)
-            else:
-                # tell the user they're a dumbass for trying to fry a non-img file type
-                result_file = None
-                text = 'Send an image file, dumbass.'
-                logging.info('sent text response: %s' % text)
-                await client.send_message(message.channel, text)
-        # the sender did not send an attachment; oops
+        img_result, text = await bot_response(message, False)
+        if not img_result:
+            await client.send_message(message.channel, text)
         else:
-            phrase = ['You forgot to take out the garbage, you ', 'You must construct additional memes, you ', 'Get your own dang meme, ', '?????, you ', 'I\'d give you a nasty look but you\'ve already got one, ', 'You are living proof that morons are a subatomic particle, ', 'You must be a cactus because you\'re a prick, you ', 'I was hoping for a battle of memes but you appear to be unarmed, you ', 'Cool story, ']
-            insult = ['dongleberry.', 'dingbat.', 'troglodyte.', 'deadhead.', 'cockalorum.', 'ninnyhammer.', 'plebian.']
-            logging.info('no image attached; sending a stock image instead')
-            msg = phrase[random.randint(0, len(phrase) - 1)] + insult[random.randint(0, len(insult) - 1)]
-            # get a random picture
-            operations = parse_args(message.content)
-            if os.path.isdir('extra/stock_images'):
-                files = glob.glob('extra/stock_images/*.*')
-                filename = files[random.randint(0, len(files) - 1)]
-                result_file, text = deepfry(filename, operations)
-                big_result = tile(filename, result_file)
-                msg += '\n' + text
-                await client.send_file(message.channel, big_result, content=msg, filename='derp.jpg')
+            await client.send_file(message.channel, img_result, content=text, filename='derp' + os.path.splitext(img_result)[1])
+
+async def bot_response(message, is_reaction):
+    if len(message.attachments) > 0:
+        logging.info('Detected an attachment to fry!')
+        logging.info('only frying first image for the time being')
+        if not os.path.isdir('/tmp/phryer'):
+            os.mkdir('/tmp/phryer')
+        result_file = '/tmp/phryer/{}'.format(message.attachments[0]['filename'])
+        logging.info('result file is: ' + result_file)
+        # retrieve the attachment
+        is_gif = False
+        is_valid = True
+        # useful properties: resp.status, resp.headers['Content-Type'], etc
+        with aiohttp.ClientSession() as session:
+            async with session.get(message.attachments[0]['url']) as resp:
+                # check response for content-type attribute
+                if resp.headers['Content-Type'] == 'image/gif':
+                    is_gif = True
+                elif resp.headers['Content-Type'] != 'image/png' and resp.headers['Content-Type'] != 'image/jpeg':
+                    is_valid = False
+                # read response into file
+                if is_valid:
+                    with open(result_file, 'wb') as the_file:
+                        async for line in resp.content:
+                            the_file.write(line)
+        # deep fry the resulting attachment
+        send_attachment = True
+        operations = parse_args(message.content)
+        if is_valid:
+            if is_gif:
+                result_file, text = mkgif(result_file, operations)
+            else:
+                result_file, text = deepfry(result_file, operations)
+            if os.path.getsize(result_file) > 7800000:
+                send_attachment = False
+                text += '\nThe file is too large to attach; RIP. '
+            if operations['chaos']:
+                send_attachment = False
+                text += ' Be advised: clicking the link may cause eye strain.'
+            if operations['link'] or not send_attachment:
+                if not creds.SUPPORT_LINKS:
+                    text += 'Harass your admin to get image links supported.'
+                else:
+                    text += make_link(result_file)
+            if send_attachment:
+                logging.info('sent text response: %s' % text)
+                return result_file, text
+            else:
+                logging.info('sent text response: %s' % text)
+                return None, text
+        else:
+            # tell the user they're a dumbass for trying to fry a non-img file type
+            result_file = None
+            text = 'Send an image file, dumbass.'
+            logging.info('sent text response: %s' % text)
+            return None, text
+    # the sender did not send an attachment; oops
+    elif not is_reaction:
+        phrase = ['You forgot to take out the garbage, you ', 'You must construct additional memes, you ', 'Get your own dang meme, ', '?????, you ', 'I\'d give you a nasty look but you\'ve already got one, ', 'You are living proof that morons are a subatomic particle, ', 'You must be a cactus because you\'re a prick, you ', 'I was hoping for a battle of memes but you appear to be unarmed, you ', 'Cool story, ']
+        insult = ['dongleberry.', 'dingbat.', 'troglodyte.', 'deadhead.', 'cockalorum.', 'ninnyhammer.', 'plebian.']
+        logging.info('no image attached; sending a stock image instead')
+        msg = phrase[random.randint(0, len(phrase) - 1)] + insult[random.randint(0, len(insult) - 1)]
+        # get a random picture
+        operations = parse_args(message.content)
+        if os.path.isdir('extra/stock_images'):
+            files = glob.glob('extra/stock_images/*.*')
+            filename = files[random.randint(0, len(files) - 1)]
+            result_file, text = deepfry(filename, operations)
+            big_result = tile(filename, result_file)
+            msg += '\n' + text
+            return big_result, msg
 
 @client.event
 async def on_ready():
